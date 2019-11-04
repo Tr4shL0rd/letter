91252d018533e840760fbd516121f5a26b2efe75
Tr4shL0rd <ketil04@gmail.com>
2019-11-04 09:52:43 +0100

    added keys per secound

--- letters.py
+++ letters.py
@@ -1,9 +1,10 @@
+
 import string, itertools, time, math, os
 
 passwordPicker = input("enter a password to guess: ")
 password = passwordPicker
 characters = string.printable
-guess = 1
+guess = 0
 def iter_all_strings():
     global start
     start = time.time()
@@ -12,13 +13,17 @@ def iter_all_strings():
         for s in itertools.product(characters, repeat=length):
             yield "".join(s)
         length +=1
+        
 for s in iter_all_strings():
         guess = guess + 1
-        print(s, guess)
+        print("{}: {}".format(guess, s))
         if s == password:
                 print('Password is {}'.format(s.upper()))
                 end = time.time()
                 elapsed = end - start
-                print("it took {} seconds and {} guesses to crack the password {}!".format(round(elapsed, 3), guess, s.upper()))
-                os.system('spd-say "your program has finished. it took {} seconds to crack the password "'.format(round(elapsed, 2)))
+                print("it took {} seconds and {} guesses to crack the password {}".format(round(elapsed, 3), guess, s.upper()))
+                os.system('spd-say "it took {} seconds and {} guesses"'.format(round(elapsed, 2), guess))
+                sumi = (guess / elapsed)
+                print("{} Keys Per Second".format(round(sumi)))
                 break
+               
